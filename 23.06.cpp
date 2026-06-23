aaa2b74c9e91e58769333dd335fb73d1d7c35a3c
Gabriel Nogueira <118150996+Bielzinng@users.noreply.github.com>
2026-06-23 19:02:57 -0300

    Update 23.06.cpp

--- 23.06.cpp
+++ 23.06.cpp
@@ -1,36 +1,41 @@
 #include <iostream>
-
 using namespace std;
 
 struct Node {
- int dado;
- Node* prox;
+    int dado;
+    Node* prox;
 };
 
 void inserirInicio(Node*& head, int valor) {
- Node* novo = new Node{valor, head};
- head = novo;
+    Node* novo = new Node{valor, head};
+    head = novo;
 }
 
 void imprimirLista(Node* head) {
- Node* atual = head;
-  
- while (atual != nullptr) 
- {
-   cout << atual->dado << " -> ";
-   atual = atual->prox;
- }
-  
- cout << "NULL";
+    Node* atual = head;
+
+    while (atual != nullptr) {
+        cout << atual->dado << " -> ";
+        atual = atual->prox;
+    }
+    cout << "NULL" << endl;
 }
 
 int main() {
- Node* head = nullptr;
-  
- inserirInicio(head, 10);
- inserirInicio(head, 20);
- inserirInicio(head, 5);
- imprimirLista(head);
-  
- return 0;
+
+    Node* head = new Node{30, nullptr};
+    head = new Node{15, head};
+
+    cout << "Lista inicial:" << endl;
+    imprimirLista(head);
+
+    inserirInicio(head, 50);
+
+    cout << "Lista após inserir 50 no início:"<< endl;
+
+    inserirInicio(head, 22);
+
+    imprimirLista(head);
+
+    return 0;
 }
